dab51770e01a4ef30ed093d282a744d43044453d
Sn1ff3hr <cy.sec526007g@gmail.com>
2025-06-15 17:45:45 -0400

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+cat > eslint.config.js <<'EOF'
+const js = require('@eslint/js');
+const prettier = require('eslint-config-prettier');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    ignores: ['node_modules', 'dist', 'build'],
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off'
+    }
+  },
+  prettier
+];
+EOF
